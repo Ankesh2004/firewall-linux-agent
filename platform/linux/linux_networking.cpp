d373901cccf0c9720e1536fbcf65437b8772c610
Ankesh2004 <gankesh808@gmail.com>
2024-09-14 17:10:05 +0530

    fix:removeInterfaceAddress

--- platform/linux/linux_networking.cpp
+++ platform/linux/linux_networking.cpp
@@ -10,10 +10,17 @@ namespace LinuxNetworking {
         return (system(command.c_str()) == 0);
     }
 
+    void removeInterfaceAddress(const std::string& interface, const std::string& address) {
+        std::string command = "ip addr del " + address + " dev " + interface;
+        if (system(command.c_str()) != 0) {
+            std::cerr << "Failed to remove address from interface: " << interface << std::endl;
+        }
+    }
+
     void setInterfaceAddress(const std::string& interface, const std::string& address) {
         if (isAddressAssigned(interface, address)) {
             std::cerr << "Address " << address << " is already assigned to interface: " << interface << std::endl;
-            return;
+            removeInterfaceAddress(interface, address);
         }
 
         std::string command = "ip addr add " + address + " dev " + interface;
