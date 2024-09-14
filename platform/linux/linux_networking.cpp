36f8c4f8f14d976696b0cca23418fdc0248a9c06
Ankesh2004 <gankesh808@gmail.com>
2024-09-14 17:03:48 +0530

    fix:setInterfaceAddress

--- platform/linux/linux_networking.cpp
+++ platform/linux/linux_networking.cpp
@@ -1,9 +1,21 @@
 #include "linux_networking.h"
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <array>
 
 namespace LinuxNetworking {
+    bool isAddressAssigned(const std::string& interface, const std::string& address) {
+        std::string command = "ip addr show " + interface + " | grep " + address;
+        return (system(command.c_str()) == 0);
+    }
+
     void setInterfaceAddress(const std::string& interface, const std::string& address) {
+        if (isAddressAssigned(interface, address)) {
+            std::cerr << "Address " << address << " is already assigned to interface: " << interface << std::endl;
+            return;
+        }
+
         std::string command = "ip addr add " + address + " dev " + interface;
         if (system(command.c_str()) != 0) {
             std::cerr << "Failed to set address for interface: " << interface << std::endl;
